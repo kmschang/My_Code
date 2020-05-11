718e29f90e624494ab40084d4344231e1dd8a8ac
kyle <kyle@schang.net>
2020-05-11 14:23:19 -0400

    Better file to check if a number is prime.

---
+++ prime.py
@@ -0,0 +1,10 @@
+check = int(input("What number do you want to check?\n:"))
+
+def is_prime(num):
+    for number in range(2,num):
+        if num % number == 0:
+            return False
+    else:
+        return True
+
+print (is_prime(check))
