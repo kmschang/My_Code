e9d10b7fa1779975b83a6495bb2e6b35dc53ee0d
kyle <kyle@schang.net>
2020-05-13 14:28:23 -0400

    fixed heads or tails

---
+++ headstailsv2.py
@@ -0,0 +1,48 @@
+from random import randint
+def poor_man_clear(num=100):
+    print ('\n'*num)
+def headstails():
+    if randint(1,3) == 1:
+        return True
+    if randint(1,3) == 2:
+        return False
+def again():
+    while True:
+        if str(input("Do you want to play again? (y or n)\n:")) == 'y':
+            return True
+        while True:
+            if str(input("Are you sure want quit? (y or n)\n:")) == 'y':
+                return False
+            else:
+                break
+        else:
+            print ("Please only type 'y' for yes and 'n' for no.")
+
+while True:
+
+    while True:
+
+        if str(input("Do you want heads or tails?\n:")) == 'heads':
+            qheads = True
+            break
+        else:
+            qheads = False
+
+            break
+
+        isheads = headstails()
+
+    if isheads == True and qheads == True:
+        print ("It was heads. You won!")
+
+    if isheads == True and qheads == False:
+        print ("It was heads. You lost!")
+
+    if isheads == False and qheads == False:
+        print ("It was tails. You won!")
+
+    if isheads == False and qheads == True:
+        print ("It was tails. You lost!")
+
+    if again() == False:
+        break
