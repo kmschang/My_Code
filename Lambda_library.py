b982add3f67844e2570bf89af8af7ee321d8988a
Kyle Schang <kyle@schang.net>
2020-05-26 18:42:30 -0400

    Worked more on lambdas

---
+++ Lambda_library.py
@@ -0,0 +1,7 @@
+mynums = [1,2,3,4,5,6,7,8,9,10]
+
+# Makes a list of even numbers
+print(list(filter((lambda num: num % 2 == 0), mynums)))
+
+# Makes a list of odd numbers
+print(list(filter((lambda num: num % 2 != 0), mynums)))
--- playground.py
+++ playground.py
@@ -12,8 +12,11 @@ for name in map(splicer,mylist):
 def check_even(num):
     return num%2 == 0
 
-mynums = [1,2,3,4,5,6,7,8]
+mynums = [1,2,3,4,5,6,7,8,9,10]
 
 for num in filter(check_even,mynums):
     print (num)
+
+
+print(list(filter((lambda num:num % 2 ==0),mynums)))
 
