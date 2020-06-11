424d98b13939a62b4416bb2a11f906310a6a7f15
Kyle Schang <kyle@schang.net>
2020-06-11 13:54:00 -0400

    Made nautical

---
+++ nautical.py
@@ -0,0 +1,17 @@
+def height_inches():
+    height = input("What is you heigh in feet and inches? (ie 6(feet),4(inches)\n:")
+    new_height = height.split(',')
+    feet = int(new_height[0])
+    inches = int(new_height[1])
+    feet_inches = feet * 12
+    inches_tall = feet_inches + inches
+    return (inches_tall)
+
+def nautical_tall():
+
+    nautial_miles_tall = height_inches() / 72913.39
+    new_height = str(nautial_miles_tall)
+    new_new_height = new_height[0:8]
+    return new_new_height
+
+print (nautical_tall())
