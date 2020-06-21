c44033d35eb126a1599a09a5635bd77e72dd31af
Kyle Schang <kyle@schang.net>
2020-06-21 12:39:41 -0400

    moved some functions around

--- Functions/function_library.py
+++ Functions/function_library.py
@@ -171,3 +171,14 @@ def square(num):
     return num**2
 
 
+def loading2():
+    import sys
+    import time
+    print("Loading progress bar...")
+    for i in range(0, 100):
+        time.sleep(0.05)
+        width = (i + 1) / 4
+        bar = "[" + "#" * int(width) + " " * (25 - int(width)) + "]"
+        sys.stdout.write(u"\u001b[1000D" + bar)
+        sys.stdout.flush()
+    print("\n")
