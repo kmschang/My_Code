63b8ff63603844bb196916436af6ae183b65e105
Kyle Schang <kyle@schang.net>
2020-06-22 13:30:10 -0400

    updated poor_man_clear

--- Function_library/poormanclear.py
+++ Function_library/poormanclear.py
@@ -1,6 +1,14 @@
 def poor_man_clear(num=100):
     print('\n'*num)
 
-num = int(input("How many spaces do you want to clear?\n:"))
+while True:
+    try:
+        num = int(input("How many spaces do you want to clear?\n:"))
+    except:
+        print("Please only input integers. Thank you")
+        num = "Try again."
+    else:
+        if num > 0 and num < 101:
+            break
 
 poor_man_clear(num)
