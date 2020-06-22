0d45ca41161386d136e45c84ce9067ad2084f1a8
Kyle Schang <kyle@schang.net>
2020-06-22 13:11:39 -0400

    updated poor_man_clear

--- Function_library/poormanclear.py
+++ Function_library/poormanclear.py
@@ -1,31 +1,6 @@
 def poor_man_clear(num=100):
-    print("\n"*num)
+    print('\n'*num)
 
+num = int(input("How many spaces do you want to clear?\n:"))
 
-def again():
-    while True:
-        question = input("Do you want to play again? (y or n)\n:")
-        if question.lower() == 'n':
-            return False
-        if question.lower() == 'y':
-            return True
-
-
-yes = True
-
-while yes == True:
-
-    while True:
-        poor_man_clear(100)
-        question2 = input("'yes' or 'no'\n:")
-        if question2.lower() == 'yes':
-            print("You lost! LOL.\n")
-            break
-        if question2.lower() == 'no':
-            print("You won! Good Job.\n")
-            break
-        else:
-            print("Please type eith 'yes' or 'no'.")
-
-    if again() == False:
-        break
+poor_man_clear(num)
