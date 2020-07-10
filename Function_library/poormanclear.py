c75c8484dc2a90861658a07ffc7083072f762d2b
Kyle Schang <kyle@schang.net>
2020-07-10 14:17:03 -0400

    added a clear function

--- Function_library/poormanclear.py
+++ Function_library/poormanclear.py
@@ -1,16 +1,21 @@
 def poor_man_clear(num=100):
+
+    while True:
+        try:
+            num = int(input("How many spaces do you want to clear?\n:"))
+        except:
+            print("Please only input integers. Thank you")
+        else:
+            if num > 0 and num < 101:
+                break
+            else:
+                print('Please only use numbers 1-100. Thank you')
+
     print('\n'*num)
 
-while True:
-    try:
-        num = int(input("How many spaces do you want to clear?\n:"))
-    except:
-        print("Please only input integers. Thank you")
-        num = "Try again."
-    else:
-        if num > 0 and num < 101:
-            break
-        else:
-            print('Please only use numbers 1-100. Thank you')
+poor_man_clear()
 
-poor_man_clear(num)
+def clear_screen():
+
+    print("\033[H\033[J")
+
