f5cbb25a815a81f081e00afd9e83d404cb384d86
Kyle Schang <kyle@schang.net>
2020-07-21 11:34:29 -0400

    working on classes

--- playground.py
+++ playground.py
@@ -2,12 +2,31 @@
 This is a playgroung file
 '''
 
-def adding():
-    '''
-    This is an adding function
-    '''
-    first = 10
-    second = 20
-    print(first + second)
+class People():
 
-adding()
+    def __init__ (self,first_name,last_name,age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = int(age)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __len__(self):
+        return self.age
+
+    def __del__(self):
+        print (f'{self.first_name} has been killed.')
+
+    def name(self):
+        print(f'My name is {self.first_name} {self.last_name}.')
+
+    def time_alive(self):
+        print(f'I am {self.age} years old.')
+
+kyle = People('Kyle','Schang','16')
+
+print(len(kyle))
+
+kyle.name()
+kyle.time_alive()
