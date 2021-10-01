f2dcda0e61585d034441a1c5397784f33ea2003a
Meaneac <Kyle@schang.net>
2021-09-30 22:00:22 -0400

    Pycharm

--- playground.py
+++ playground.py
@@ -1,19 +1,18 @@
-'''
+"""
 This is a playgroung file
-'''
-n = 0
+"""
 
 from random import randint
+
+n = 0
 
 numbers = []
 means = []
 meanlist = []
-x=0
-
-while x < 100:
+for _ in range(100):
     while n < 10:
-        numbers.append(randint(0,100))
-        n+=1
+        numbers.append(randint(0, 100))
+        n += 1
 
     while len(numbers) > 1:
         newnumber = numbers[0] + numbers[1]
@@ -21,11 +20,9 @@ while x < 100:
         numbers.pop(0)
         numbers.pop(0)
 
-    means.append (numbers[0]/n)
-    n=0
+    means.append(numbers[0]/n)
+    n = 0
 
     numbers = []
     newnumber = 0
-    x+=1
-    
 print(means)
