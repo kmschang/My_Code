c176eb650ca9aefafbb627cec5ddd5bf6db66387
Meaneac <Kyle@schang.net>
2021-09-30 22:26:33 -0400

    Playing around

--- playgroundv2.py
+++ playgroundv2.py
@@ -5,22 +5,17 @@ from random import randint
 numbers = []
 addition = []
 meanlist = []
-x=0
-
-while x < 100:
+newnumber = 0
+for _ in range(100):
     while n < 10:
-        numbers.append(randint(0,100))
-        n+=1
+        numbers.append(randint(0, 100))
+        n += 1
 
     addition = sum(numbers)
     mean = addition/n
     meanlist.append(mean)
 
-    n=0
+    n = 0
 
     numbers = []
-    newnumber = 0
-    x+=1
-
-
 print(meanlist)
