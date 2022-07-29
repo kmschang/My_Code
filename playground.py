a40f63f0ce9c780f72d6f562fb50f1a6e3923eaf
Meaneac <kyle@schang.net>
2022-07-29 13:14:15 -0400

    making the game more compact and runable

--- playground.py
+++ playground.py
@@ -1,1 +1,9 @@
 print ("heillo")
+
+def subtractoin(num1,num2):
+
+    answer = num1 - num2
+    print(answer)
+
+
+subtractoin(1,2)
--- playgroundv4.py
+++ playgroundv4.py
@@ -1,1 +1,46 @@
-## This is a test
+from random import randint
+
+
+def get_name():
+    return str(input("What is your name?\n:")).capitalize()
+
+
+def welcome_screen(name):
+    print(f"Welcome {name}")
+    print("You are playing: How close to a random number 1 -100!")
+
+
+def guess():
+    while True:
+        try:
+            question = int(input("Pick a number 1-100\n:"))
+        except:
+            print("Please only input integers. Thank you.")
+            question = "Try again."
+        else:
+            if 0 < question < 101:
+                return question
+            else:
+                print('Please only input integers 1-100. Thank you.')
+
+
+def random_number():
+    comp_num = int(randint(0, 100))
+    return comp_num
+
+
+def game():
+    name = get_name()
+
+    welcome_screen(name)
+
+    player_guess = guess()
+    computer_guess = random_number()
+
+    diff = abs(player_guess - computer_guess)
+    print(f"Computer: {computer_guess}, {name}: {player_guess}")
+    print(f"The difference between the numbers was {diff}!")
+
+
+game()
+
