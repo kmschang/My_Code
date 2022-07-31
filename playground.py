3a622592ec8088bdbb1776a37136a10053538737
Meaneac <kyle@schang.net>
2022-07-31 15:49:22 -0400

    tweeking the output to file

--- playground.py
+++ playground.py
@@ -1,9 +1,25 @@
 from random import randint
 
+def again():
+    while True:
+        try:
+            yorn = int(input("\nDo you want to play again? (1 = yes or 0 = no)\n:"))
+        except:
+            print("Please only input 1 for yes or 0 for no. Thank you.")
+        else:
+            if yorn == 1:
+                return True
+            elif yorn == 0:
+                return False
+            else:
+                print("Please only input 0 or 1.")
+
+
+name = str(input("What is your name?\n:"))
+name = name.capitalize()
+
 while True:
 
-    name = str(input("What is your name?\n:"))
-    name = name.capitalize()
     player_output = int(input("Choose a number 1-10\n:"))
 
     count = 0
@@ -18,9 +34,10 @@ while True:
         if player_output - computer_output == 0:
             print(f"\u001b[32;5mTook {count} times to be equal.\u001b[31;0m")
             with open("playground_outputs.txt", "a") as myfile:
-                myfile.write(f"\n{name}: {count}")
+                myfile.write(f"\n{name}: {count} ({player_output})")
             break
         else:
             count += 1
 
-    break
+    if not again():
+        break
