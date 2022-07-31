7ed68e2b49486ac7bbc1f2181fd5a4b4bd2dde2d
Meaneac <kyle@schang.net>
2022-07-31 15:27:27 -0400

    added colors

--- playground.py
+++ playground.py
@@ -1,9 +1,22 @@
-print ("heillo")
+from random import randint
 
-def subtractoin(num1,num2):
+while True:
 
-    answer = num1 - num2
-    print(answer)
+    player_output = int(input("Choose a number 1-10\n:"))
 
+    count = 0
 
-subtractoin(1,2)
+    while True:
+
+        computer_output = randint(1, 10)
+
+        print(f"Computer: {computer_output}, Player: {player_output}")
+        print(f"Difference:\u001b[31;1m {player_output - computer_output}\n\u001b[31;0m")
+
+        if player_output - computer_output == 0:
+            print(f"\u001b[32;5mTook {count} times to be equal.\u001b[31;0m")
+            break
+        else:
+            count += 1
+
+    break
