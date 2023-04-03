11624c1a8d010fc1498e339bfc2ad40caf7b0903
Meaneac <Kyle@schang.net>
2023-04-03 17:43:22 -0400

    formatting

--- Games/binary.py
+++ Games/binary.py
@@ -1,23 +1,22 @@
 def binary():
-
-    x = "{0:b}".format(
-        1859718347812739481235781674623847901283498123948213567861271083567816308478934708917238496357106982374897123896571620849723894782365781032784682342434689027)
+    x = "{:b}".format(
+        1859718347812739481235781674623847901283498123948213567861271083567816308478934708917238496357106982374897123896571620849723894782365781032784682342434689027
+    )
 
     while True:
-        if x ==1:
-            x = '\u001b[37; 1m' + x
-        if x ==0:
-            x = '\u001b[37;1m' + x
-        print (x)
+        if x == 1:
+            x = "\u001b[37; 1m" + x
+        if x == 0:
+            x = "\u001b[37;1m" + x
+        print(x)
         x = x * 7
 
-def counter():
 
+def counter():
     x = 1
 
     while True:
-
-        print (x)
+        print(x)
         x += 1
 
 
@@ -29,3 +28,5 @@ elif ques.lower() == "counter":
     print(counter())
 else:
     print("There is nothing like that here. Please try again.")
+
+    # this is a test
