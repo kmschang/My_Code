163e5a077e2683eddc496ba698eac41c6367c812
Meaneac <Kyle@schang.net>
2023-04-04 20:22:54 -0400

    ran black on all files

---
+++ scripts/black.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# This script is used to format the code using black.
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Change to the root directory of the project.
+cd "$(dirname "$0")/.."
+
+# Format the code.
+black .
